7c29aed84d72845fca1b185c04280a1cdaee8c22
Mario <Mario@PC-Pro>
2020-03-09 18:47:43 +0100

    Primer Commit

---
+++ ED_Tarea4/src/practicaunidad4/CCuenta.java
@@ -0,0 +1,114 @@
+﻿/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practicaunidad4;
+
+
+public class CCuenta {
+
+   private String nombre;
+   private String cuenta;
+   private double saldo;
+   private double tipoInterés;
+   
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
+    
+    public CCuenta ()
+    {
+    }
+    
+    public CCuenta (String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+        tipoInterés=tipo;
+    }
+   
+    public void asignarNombre(String nom)
+    {
+        nombre=nom;
+    }
+    
+    
+
+    
+     public double estado ()
+    {
+        return saldo;
+    }
+
+    
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0){
+            throw new Exception("No se puede ingresar una cantidad negativa");}
+        setSaldo(saldo + cantidad);
+    }
+
+
+
+    
+    public void retirar (double cantidad) throws Exception
+    {
+        if (cantidad < 0){
+            throw new Exception ("No se puede retirar una cantidad negativa");}
+        if (estado()< cantidad){
+            throw new Exception ("No se hay suficiente saldo");}
+        setSaldo(saldo - cantidad);
+    }
+
+    public void operativa_cuenta(CCuenta miCuenta,float ingresar, int opcion,float cantidad){
+        if(opcion==2){
+            try 
+            {
+                miCuenta.retirar(cantidad);
+            } catch (Exception e)
+            {
+                System.out.print("Fallo al retirar");
+            }
+        }else if(opcion==1){
+            try
+            {
+                System.out.println("Ingreso en cuenta");
+                miCuenta.ingresar(cantidad);
+            } catch (Exception e)
+            {
+                System.out.print("Fallo al ingresar");
+            }
+        }
+    }
+    
+}
---
+++ ED_Tarea4/src/practicaunidad4/Main.java
@@ -0,0 +1,72 @@
+﻿/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practicaunidad4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+
+ */
+public class Main {
+    static BufferedReader dato = new BufferedReader(new InputStreamReader(System.in));
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+        int opcion=0;
+        cuenta1 = new CCuenta("Juan López","1000-2365-85-123456789",2500,0);
+        do{
+            try {
+                System.out.println("MENÚ DE OPERACIONES");
+                System.out.println("-------------------");
+                System.out.println("1 - Ingresar");
+                System.out.println("2 - Retirar");
+                System.out.println("3 - Finalizar");
+                opcion=Integer.parseInt(dato.readLine());
+                if(opcion==1){
+                    System.out.println("¿Cuánto desea ingresar?: ");
+                    float ingresar=Integer.parseInt(dato.readLine());
+                    cuenta1.operativa_cuenta(cuenta1,ingresar,opcion, ingresar);
+                }else if(opcion==2){
+                    System.out.println("¿Cuátno desea retirar?: ");
+                    float retirar=Integer.parseInt(dato.readLine());
+                    cuenta1.operativa_cuenta(cuenta1,retirar,opcion, retirar);
+                }else if(opcion==3){
+                    System.out.println("Finalizamos la ejecución");
+                }else{
+                    System.err.println("Opción errónea");
+                }
+/*                switch(opcion){
+                    case 1:
+                        System.out.println("¿Cuánto desea ingresar?: ");
+                        float ingresar=Integer.parseInt(dato.readLine());
+                        operativa_cuenta(miCuenta,ingresar,opcion);
+                        break;
+                    case 2:
+                        System.out.println("¿Cuátno desea retirar?: ");
+                        float retirar=Integer.parseInt(dato.readLine());
+                        operativa_cuenta(miCuenta,retirar,opcion);
+                        break;
+                    case 3:
+                        System.out.println("Finalizamos la ejecución");
+                }
+*/            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }while (opcion!=3);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+    }
+
+}
